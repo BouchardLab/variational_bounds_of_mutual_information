de189586cbf99ad18ae5a85fd73cbd4be3c20934
Ji Hyun Bak <jihyun.bak@gmail.com>
2022-02-10 10:35:35 -0800

    make an installable package vbmi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vbmi',
+    version='0.0.1.dev0',
+    description=('Variational Bounds of Mutual Information'),
+    url='',
+    packages=find_packages(),
+)
